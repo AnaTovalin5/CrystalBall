21ef79780e703bc5fb839e4ce9c069510543a68a
Ana Tovalin <astrostar98@gmail.com>
2014-09-12 14:31:47 -0700

    Commit after finishing all the beginner Milestones

--- app/src/main/java/android/tovalina/crystalball/CrystalBall.java
+++ app/src/main/java/android/tovalina/crystalball/CrystalBall.java
@@ -1,12 +1,15 @@
 package android.tovalina.crystalball;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class CrystalBall extends Activity {
@@ -14,6 +17,7 @@ public class CrystalBall extends Activity {
     private TextView answerText;
 
     private SensorManager sensorManager;
+    private Sensor accelerometer;
     private float acceleration;
     private float currentAcceleration;
     private float previousAcceleration;
@@ -21,7 +25,19 @@ public class CrystalBall extends Activity {
     private final SensorEventListener sensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
 
+            previousAcceleration = currentAcceleration;
+            currentAcceleration = FloatMath.sqrt(x * x + y * y + z * z);
+            float delta = currentAcceleration - previousAcceleration;
+            acceleration = acceleration * 0.9f + delta;
+
+            if(acceleration > 15) {
+                Toast toast = Toast.makeText(getApplication(), "Device has shaken", Toast.LENGTH_SHORT);
+                toast.show();
+            }
         }
 
         @Override
@@ -35,8 +51,26 @@ public class CrystalBall extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crystal_ball);
 
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        acceleration = 0.0f;
+        currentAcceleration = sensorManager.GRAVITY_EARTH;
+        previousAcceleration = sensorManager.GRAVITY_EARTH;
+
         answerText = (TextView) findViewById(R.id.answerText);
         answerText.setText(Predictions.get().getPrediction());
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorListener);
+    }
 }
