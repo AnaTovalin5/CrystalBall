6367c74b2120ce35872d2671ea1167be66b2363b
Ana Tovalin <astrostar98@gmail.com>
2014-09-16 14:43:06 -0700

    Commit after completing Intermediate Milestone 4

--- app/src/main/java/android/tovalina/crystalball/predictions.java
+++ app/src/main/java/android/tovalina/crystalball/predictions.java
@@ -2,24 +2,24 @@ package android.tovalina.crystalball;
 
 public class Predictions {
 
-    private static Predictions predictions;
+    private static Predictions predictions; //setting variables
     private String[] answers;
 
     private Predictions() {
-        answers = new String[] {
+        answers = new String[] { //contains all predictions
                 "Your wishes will come true.",
                 "Your wishes will NEVER come true."
         };
     }
 
-    public static Predictions get() {
+    public static Predictions get() { //Lets the prediction be able to be used after the intial start of the program
         if(predictions == null) {
             predictions = new Predictions();
         }
         return predictions;
     }
 
-    public String getPrediction() {
+    public String getPrediction() { //determines which fortune will show on the screen
         return answers[1];
     }
 }
