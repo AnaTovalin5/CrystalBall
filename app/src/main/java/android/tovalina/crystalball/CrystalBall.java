4752ec71389a783797be9314852e1651cd72916e
Ana Tovalin <astrostar98@gmail.com>
2014-09-17 15:03:36 -0700

    Commit after Intermediate Milestone 7
    Play sound on shake

--- app/src/main/java/android/tovalina/crystalball/CrystalBall.java
+++ app/src/main/java/android/tovalina/crystalball/CrystalBall.java
@@ -6,6 +6,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.FloatMath;
 import android.widget.TextView;
@@ -35,8 +36,8 @@ public class CrystalBall extends Activity {
             acceleration = acceleration * 0.9f + delta;
 
             if(acceleration > 15) {
-                Toast toast = Toast.makeText(getApplication(), "Device has shaken", Toast.LENGTH_SHORT);
-                toast.show();
+                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.crystal_ball);
+                mediaPlayer.start();
             }
         }
 
